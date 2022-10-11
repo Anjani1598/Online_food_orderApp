4383bf1befcfb0071760fc786b4d9f5d8711555f
Kousik1234 <kousikmanik8945@gmail.com>
2022-10-11 21:59:19 +0530

    end

--- OnlineFoodDeliveryApp/src/main/java/com/masai/controller/CustomerController.java
+++ OnlineFoodDeliveryApp/src/main/java/com/masai/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.masai.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.masai.Model.Customer;
+import com.masai.model.Customer;
 import com.masai.exceptions.CustomerException;
 import com.masai.service.icustomerService.ICustomerService;
 
